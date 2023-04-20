102c0a8feff66b1bd53aae85938d26d15b3c1399
Denis Kolodin <deniskolodin@gmail.com>
2023-04-20 18:50:44 +0200

    Release 0.3.1

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use rustinsight::opts::Opts;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let opts = Opts::parse();
+    println!("Install the new version (0.4.0) of the launcher app:");
+    let command = "cargo install knowledge --force".red();
+    println!("$ {command}");
+    println!("");
     if let Err(err) = App::entrypoint(opts, false).await {
         let err = err.to_string().red();
         println!("Failed: {err}",);
